89b22be32e8e836ad6cdfa724cb2c6401e83d902
= <=>
2023-01-15 20:50:46 +0100

    twitter.py some functions, most important send tweet

---
+++ functionalities/twitter.py
@@ -0,0 +1,64 @@
+import tweepy
+from dotenv import load_dotenv
+import os
+from typing import List, Union
+
+load_dotenv()
+api_key = os.getenv('TWITTER_API_KEY')
+api_secret = os.getenv('TWITTER_API_KEY_SECRET')
+access_token = os.getenv('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+bearer_token = os.getenv('TWITTER_BEARER_TOKEN')
+
+def get_client():
+    client = tweepy.Client(
+        bearer_token,
+        api_key,
+        api_secret,
+        access_token,
+        access_token_secret)
+    return client
+
+def get_api():
+    auth = tweepy.OAuth1UserHandler(
+        api_key,
+        api_secret,
+        access_token,
+        access_token_secret
+    )
+    return tweepy.API(auth)
+
+def send_tweet(tweet_text: str, media_url: str = None):
+    client = get_client()
+    extra_args = {}
+    if media_url is not None:
+        api = get_api()
+        media = api.media_upload(filename=media_url)
+        extra_args["media_ids"] = [media.media_id_string]
+    client.create_tweet(text=tweet_text, user_auth=True, **extra_args)
+    print("tweet Successful!")
+
+
+def delete_tweet(id_list: Union[int, List[int]]):
+    client = get_client()
+    list(map(lambda i: client.delete_tweet(i, user_auth=True), id_list))
+    print("Deleted tweet succesful")
+
+
+def get_client_info():
+    client = get_client()
+    return client.get_me()
+
+
+def get_tweets(username=None):
+    client = get_client()
+    if username is None:
+        username = client.get_me().data["username"]
+    tweets = client.search_recent_tweets(query=f"from:{username}", user_auth=True, tweet_fields=["created_at"]).data
+    result = {}
+    if tweets is None:
+        return result
+    for tw in tweets:
+        result[tw.id] = {}
+        result[tw.id] = {'text': tw.text, 'utc_time_post': tw.created_at.strftime("%d/%m/%Y, %H:%M:%S")}
+    return result
